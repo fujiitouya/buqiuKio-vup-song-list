e5f31c81e680cf89aa8187f3cecb166073580ff1
Fujii Touya <48616819+fujiitouya@users.noreply.github.com>
2023-11-05 23:54:16 +0800

    First Commit
    Initial Commit

---
+++ config/constants.js
@@ -0,0 +1,42 @@
+let config = {
+    Name: "步秋Kio",
+    
+    BiliLiveRoomID: "30524278",
+    
+    NetEaseMusicId: "",
+    QQMusicId: "",
+    Footer: "Copyright © 2023 步秋Kio 解释权归本直播间所有。",
+
+    Cursor: true,
+
+    LanguageCategories: ["国语", "英语", "韩语", "粤语","日语"],
+    RemarkCategories: [],
+
+    BannerTitle: "雪域provealms所属古神教的祭司步秋",
+    BannerContent: [
+`     ♡大家好，我是来自古神教的祭司步秋⋆꙳
+
+自从继任祭司以来已经好久没有离开过遗迹了，看着起源星的大家都连接上异世界也认识了你们，真的很羡慕呢，所以这次我来啦！(*‘ω‘ *)
+
+（虽然是名祭司，不过我侍奉的古神好像不太以神明自称，所以就暂且不传教了！希望可以和大家相处愉快˚✧₊)
+     解释权归本直播间所有。`,
+     ],
+
+    CustomButtons: [
+        {
+            link: "mailto:provealms@foxmail.com",
+            name: "商务合作",
+            image: ""
+        },
+        {
+            link: "https://space.bilibili.com/17100532/",
+            name: "个人主页",
+            image: ""
+        }
+    ]
+}
+
+
+module.exports = {
+    config
+}
